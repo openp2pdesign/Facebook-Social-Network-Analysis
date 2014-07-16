a95cb97d6575101eb6cdf3ca0a9b3a57f18d3ffb
Massimo Menichinelli <info@openp2pdesign.org>
2014-07-16 11:22:43 +0200

    Fix group friendships

--- snafacebook-group-friendships.py
+++ snafacebook-group-friendships.py
@@ -18,7 +18,17 @@ import requests
 import json
 import networkx as nx
 from time import sleep
+import itertools
 import os
+
+# From http://stackoverflow.com/a/312467
+def split_seq(iterable, size):
+    it = iter(iterable)
+    item = list(itertools.islice(it, size))
+    while item:
+        yield item
+        item = list(itertools.islice(it, size))
+
 
 # Clear screen
 os.system('cls' if os.name=='nt' else 'clear')
@@ -34,7 +44,8 @@ fab9 = "152023668302761"
 maind = "51802632393"
 
 group = fii
-base_url = 'https://graph.facebook.com/'+group+'/members'
+group = maind
+base_url = 'https://graph.facebook.com/v1.0/'+group+'/members'
 
 # How to get an infinite access token, otherwise our analysis takes too long
 #
@@ -85,9 +96,35 @@ print "Getting all the users..."
 
 end_point = "https://graph.facebook.com/v1.0/"
 
+batch_request = ""
+
+users = []
+
+newpage = {}
+i = 0
+newpage[0] = requests.get(url).json()
+
+while 1 > 0:
+	print "-----------------------------------------"
+	print "Reading page n.", i,"from the Facebook API..."
+	print ""
+	if i != 0:
+		newpage[i] = requests.get(newpage[i-1]["paging"]["next"]).json()
+	if len(newpage[i]["data"]) == 0:
+		print "Got all the data."
+		break
+	if "paging" in newpage[i].keys():
+		for w,x in enumerate(newpage[i]["data"]):
+			users.append(x)
+
+	i = i + 1
+
+print ""
+print "There are",len(users),"users."
+
 # Get the users and the connections between the users
-for i,k in enumerate(content["data"]):
-	base_url3 = 'https://graph.facebook.com/'+k["id"]
+for i,k in enumerate(users):
+	base_url3 = 'https://graph.facebook.com/v1.0/'+k["id"]
 	fields3 = '&limit=2000'
 	url3 = '%s?fields=%s&access_token=%s' % (base_url3, fields3, INFINITE_TOKEN)	
 	
@@ -124,24 +161,25 @@ for i,k in enumerate(content["data"]):
         	# print firstname
 
 		# Add node with gender data
-		graph.add_node(X)
+		if X not in graph.nodes():
+			graph.add_node(X)
 		if "gender" in firstname:
-			graph[X]['gender']=firstname["gender"]
+			graph.node[X]['gender']=firstname["gender"]
+			print X,"is a",graph.node[X]['gender']
 		else:
-			graph[X]['gender']= "None"
+			graph.node[X]['gender']= "None"
 	
 		# Check friendships with the other members of the group:		
 		# Creating requests
 		all_requests = []
-		for j,m in enumerate(content["data"]):
+		for j,m in enumerate(users):
 			single_request= {'method':'GET', 'relative_url': k['id']+'/friends/'+m['id']}
 			all_requests.append(single_request)
 		
 	# Making batch requests
 	# https://developers.facebook.com/docs/graph-api/making-multiple-requests/
 	# Split the list of requests in blocks of 50 requests, the limit for batch requests
-	# Got this list split from http://stackoverflow.com/a/9671264
-	chunks = [all_requests[50*i:50*(i+1)] for i in range(len(all_requests)/50)]
+	chunks = split_seq(all_requests, 50)
 	
 	for u in chunks:
 		batch_request = json.dumps(u)			
@@ -149,6 +187,7 @@ for i,k in enumerate(content["data"]):
 			facebook_request = requests.post(end_point, params={'access_token': INFINITE_TOKEN, 'batch': batch_request})
 			#result_string = json.loads(facebook_request.text.strip("'<>() ").replace('\'', '\"'))
 		except: 
+			print ""
 			print "There was an error. Waiting 30 minutes before starting again..."
 			sleep(60*30)
 			facebook_request = requests.post(end_point, params={'access_token': INFINITE_TOKEN, 'batch': batch_request})
@@ -161,14 +200,15 @@ for i,k in enumerate(content["data"]):
 		if "error" in result_string:
                 	# Wait 30 minutes if we reach the user limit
                         # See https://developers.facebook.com/docs/reference/ads-api/api-rate-limiting/
-                        print "There was an error. Waiting 30 minutes before starting again..."
+                        print ""
+			print "There was an error. Waiting 30 minutes before starting again..."
                         sleep(60*30)
 
                         facebook_request = requests.post(end_point, params={'access_token': INFINITE_TOKEN, 'batch': batch_request})
                         result_string = json.loads(facebook_request.text)
 
 		for z in result_string:
-			if z["body"] is not None or z is not None:
+			if z is not None and z["body"] is not None:
 				result_dict = json.loads(z["body"])
 				# Debug
 				# print result_dict
@@ -183,4 +223,4 @@ print ""
 print "The group was analyzed succesfully."
 print ""
 print "Saving the file as "+group+"-friendships.gexf..."
-nx.write_gexf(graph, group+"-friendships.gexf")
+nx.write_gexf(graph,group+"-friendships.gexf")
